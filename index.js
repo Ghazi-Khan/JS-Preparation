5058ef8df9142e085b60e42c31841491cc853cf9
Ghazi Khan <mailme.gkhan@gmail.com>
2022-03-18 15:55:12 +0530

    Challenge # 1 — Counting duplicate characters

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Test');
+// Challenge # 1 — Counting duplicate characters
+
+// Given a string you need to find out the duplicate characters in the string.
+
+// Input: “adsjfdsfsfjsdjfhacabcsbajda”
+// Output: { a: 5, b: 2, c: 2, d: 4, f: 4, j: 4, s: 5 }
+
+// This fuc will retunr -1 if the string is invalid
+// else will retunr count of characters
+const conuntDuplicateChars = (str) => {
+  if (!str) return -1;
+
+  const res = {};
+  // Traditional for-loop
+  // for (let i = 0; i < str.length; i++) {
+  //     const char = str[i];
+  //     if (res[char]) {
+  //         res[char]++;
+  //     } else {
+  //         res[char] = 1;
+  //     }
+  // }
+
+  // using forEah-loop
+  str.split('').forEach((char) => {
+    if (res[char]) {
+      res[char]++;
+    } else {
+      res[char] = 1;
+    }
+  });
+  return res;
+};
+
+const test = "adsgfagadsgdaffdsAASFCASD";
+console.log("Count Duplicate characters: ", conuntDuplicateChars(test));
