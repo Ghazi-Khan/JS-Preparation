80040260dde7454cd7859d8294ced98c0a98231a
Harsh Jain <hvj.techture@gmail.com>
2022-03-19 10:32:36 +0530

    list of array operations added

---
+++ Array.js
@@ -0,0 +1,14 @@
+// Most common methods =>
+
+// 1. map() -> used for transforamtion of array
+// 2. filter() -> used filteration of array that needed from array.
+// 3. reduce(acc,curr) ->
+// 4. splice() -> used for replacing / deleting items
+// 5. find() -> returns first value that it find in array. if value is not found then return undefined.
+// 6. findIndex() -> return index position of item if item is not found return -1
+// 7. concat() -> add two arrays
+// 8. slice(startPositon,endPositon) -> deletes item from array.
+// 9. indexof(item) -> return index position
+//10. lastindexof(item) -> returns last index positon
+//11. includes(itemName) -> return true if item is found else false.
+//12. sort() -> sort the data lexographically.
