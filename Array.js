0e417e3730ca9eb3134ecd751d339677bf0a77f9
Harsh Jain <hvj.techture@gmail.com>
2022-03-19 10:36:27 +0530

    definiton added for reduce

--- Array.js
+++ Array.js
@@ -1,8 +1,8 @@
 // Most common methods =>
 
-// 1. map() -> used for transforamtion of array
-// 2. filter() -> used filteration of array that needed from array.
-// 3. reduce(acc,curr) ->
+// 1. map() -> is used for transformation of each elements of array.
+// 2. filter() -> is used to filter elements of array based on some condition.
+// 3. reduce(acc,curr) -> is used to find out some result based on the elements of the array, like sum of all element, max among elements, so on.
 // 4. splice() -> used for replacing / deleting items
 // 5. find() -> returns first value that it find in array. if value is not found then return undefined.
 // 6. findIndex() -> return index position of item if item is not found return -1
